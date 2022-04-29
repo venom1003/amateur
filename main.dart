9c910f8122e292b4cbf56946aeefd090547cc526
Vinamra Mahajan <104397548+venom1003@users.noreply.github.com>
2022-04-30 01:59:47 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: use_key_in_widget_constructors, annotate_overrides, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text('Hello!'),
+    );
+  }
+}
