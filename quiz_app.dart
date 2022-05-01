266ed424c4fceac5bd9ff94a9eb3804737a16ce6
Vinamra Mahajan <104397548+venom1003@users.noreply.github.com>
2022-05-01 23:34:11 +0530

    Add files via upload

---
+++ quiz_app.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    var questions = [
+      "What's your favorite color?",
+      "What's your favorite animal",
+    ];
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('My First App'),
+          ),
+          body: Column(children: [
+            Text("The question!"),
+            RaisedButton(child: Text('Answer 1'), onPressed: null),
+            RaisedButton(child: Text('Answer 2'), onPressed: null),
+            RaisedButton(child: Text('Answer 3'), onPressed: null),
+            RaisedButton(child: Text('Answer 4'), onPressed: null),
+          ])),
+    );
+  }
+}
