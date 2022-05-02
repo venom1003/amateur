fb2e3dafb0a4845bf02f6432a87c0f624a4b9438
Vinamra Mahajan <104397548+venom1003@users.noreply.github.com>
2022-05-03 03:45:08 +0530

    Add files via upload

---
+++ quiz_app_update1.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // ignore: todo
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex += 1;
+    });
+  }
+
+  Widget build(BuildContext context) {
+    var questions = [
+      "What's your favorite color?",
+      "What's your favorite animal?",
+    ];
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('My First App'),
+          ),
+          body: Column(children: [
+            Text(questions[questionIndex]),
+            ElevatedButton(child: Text('Answer 1'), onPressed: answerQuestion),
+            ElevatedButton(
+                child: Text('Answer 2'),
+                onPressed: () => print('Answer 2 chosen!')),
+            ElevatedButton(
+                child: Text('Answer 3'),
+                onPressed: () {
+                  // ..
+                  print('Answer 3 chosen!');
+                }),
+            ElevatedButton(child: Text('Answer 4'), onPressed: answerQuestion),
+          ])),
+    );
+  }
+}
