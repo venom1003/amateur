d3adb4fb1ba643e0aa8c58ce373d9c528a140e1a
Vinamra Mahajan <104397548+venom1003@users.noreply.github.com>
2022-05-05 00:49:51 +0530

    Add files via upload

---
+++ question.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: must_be_immutable, use_key_in_widget_constructors, avoid_unnecessary_containers, sized_box_for_whitespace
+
+import 'package:flutter/material.dart';
+
+class Question extends StatelessWidget {
+  final String questionText;
+
+  const Question(this.questionText);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: double.infinity,
+        margin: const EdgeInsets.all(10),
+        child: Text(
+          questionText,
+          style: const TextStyle(fontSize: 28),
+          textAlign: TextAlign.center,
+        ));
+  }
+}
