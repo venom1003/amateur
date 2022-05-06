2ce548f64058914958b06d4edccadc627040c00a
Vinamra Mahajan <104397548+venom1003@users.noreply.github.com>
2022-05-06 23:11:14 +0530

    Add files via upload

---
+++ answer.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: unused_import, use_key_in_widget_constructors, deprecated_member_use, avoid_web_libraries_in_flutter, sized_box_for_whitespace
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+  final VoidCallback selectHandler;
+  final String answerText;
+
+  const Answer(this.selectHandler, this.answerText);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: RaisedButton(
+        color: Colors.blue,
+        textColor: Colors.white,
+        child: Text(answerText),
+        onPressed: selectHandler,
+      ),
+    );
+  }
+}
